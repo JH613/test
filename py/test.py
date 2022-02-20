8b1f2f00ec406ebcabd739acfa95e1c67171f447
JH613 <qswnsgus309@naver.com>
2022-02-20 22:36:22 +0900

    PY_test

--- py/test.py
+++ py/test.py
@@ -1,4 +1,54 @@
-test_list = ['1','2','3',[1,2,3,4,5,6,7,8]]
-for a in test_list:
-  print(a)
-  print(type(a[0]))
+#%% case 1
+test_score = [100, 20, 40 ,37, 41]
+num = 0
+for a in test_score :
+  num = num + 1
+  if a >= 60:
+    print("%d번 합격." % num)
+  else:
+   print("%d번 불합격." % num)
+
+
+# %%
+marks = [90, 25, 67, 45, 80]
+
+number = 0 
+for mark in marks: 
+    number = number +1 
+    if mark >= 60: 
+        print("%d번 학생은 합격입니다." % number)
+    else: 
+        print("%d번 학생은 불합격입니다." % number)
+# %%
+treeHit = 0
+while treeHit <100 :
+ treeHit = treeHit +1 
+ print("나무를 %d번 찍었습니다" % treeHit)
+ if treeHit == 100:
+   print("김석환이 죽습니다")
+# %%
+pocket = ['', 'smartphone','paper' ]
+if 'momey' in pocket:
+    print("택시를 타고 귀가")
+else:
+      print("돈을 뺏어서 택시를 타고 귀가")
+
+# %%
+a = "Life is too short, you need python"
+
+if "wife" in a: print("wife")
+elif "python" in a and "you" not in a: print("python")
+elif "shirt" not in a: print("shirt")
+elif "need" in a: print("need")
+else: print("none")
+# %%
+result = 0
+i = 1
+while i <= 1000:
+    if i % 3 == 0: 
+        result += i
+    i += 1
+
+print(result) 
+
+# %%
